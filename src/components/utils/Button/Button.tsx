7cc308c32210266b37080c24a4b10c29d1806f06
Samir Dewan <samirdewan1995@gmail.com>
2023-09-18 18:09:10 +0100

    default button add

---
+++ src/components/utils/Button/Button.tsx
@@ -0,0 +1,17 @@
+import "./Button.css";
+
+interface ButtonProps {
+    text: string,
+    onClick: () => void,
+    uppercase?: boolean,
+}
+
+export default function Button({text, onClick, uppercase}: ButtonProps) {
+    return (
+        <button
+        className="DefaultButton"
+        onClick={onClick}>
+            {uppercase ? text : text}
+        </button>
+    )
+}
